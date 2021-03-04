e9c8d439d8a836d44c7c7060942f94d691a17c87
smithmyname <35342884+smithmyname@users.noreply.github.com>
2021-03-03 22:08:38 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+// when the game ends, show the results component
+// output the score inside the results component
+import Block from './components/Block'
+import Results from './components/Results'
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      // set time amount (ms)
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button {
+  background: slategray;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 30px;
+}
+
+button [disabled] {
+  opacity: 0.7;
+  cursor: not-allowed;
+}
+
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Lightning Fast!'
+    } else if (this.score < 400) {
+      this.rank = 'Pretty Quick'
+    } else {
+      this.rank = 'Pokey Pace'
+    }
+  }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.6rem;
+        color:rgb(51, 83, 224);
+        font-weight: bold;
+    }
+</style>
